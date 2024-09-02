04fa2fdaf4a94e73b687c9c42ad74d90950a8b62
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-09-02 07:57:30 -0400

    added text to image section and started sd setup in utils.py

--- imagealter.py
+++ imagealter.py
@@ -8,31 +8,85 @@ background:radial-gradient(#416e8a, #000000);
 
 with gr.Blocks(theme="default", fill_width=True, css=css) as demo:
     gr.Markdown("# <center>Image Alter</center>")
-    gr.Markdown("<center>This app is used to edit images using Stable Diffusion.</center>")
-    gr.Tabs()
-    with gr.Row():
-        with gr.Column(scale=4, show_progress=True):
-            gr.Markdown("## <center>Input Image</center>")
-            gr.Tabs()
-            gr.Image(height="50vh", show_label=False)
-        with gr.Column(scale=3, show_progress=True, variant="compact"):
-            gr.Chatbot(height="50vh")
-            gr.Textbox(placeholder="Enter image edit prompt...")
-        with gr.Column(scale=4, show_progress=True):
-            gr.Markdown("## <center>Output Image</center>")
-            gr.Tabs()
-            gr.Image(height="50vh", show_label=False, interactive=False)
-    gr.Markdown("# <center>Output Image Gallery</center>")
-    gr.Tabs()
-    gr.Gallery(height="500",
-               rows=[6],
-               columns=[6],
-               show_download_button=True,
-               show_fullscreen_button=True,
-               preview=True,
-               label="Output Image Gallery",
-               show_label=False,
-               object_fit="scale-down")
+    gr.Markdown("### <center>This app is used to create and edit images using Stable Diffusion.</center>")
+    with gr.Tab("Text to Image"):
+        with gr.Row():
+            with gr.Column(scale=2, show_progress=True, variant="compact"):
+                num_images = gr.Slider(minimum=1, maximum=5, value=1, step=1,
+                                       label="Number of Images to Generate",
+                                       info="How many images you want the model to generate.",
+                                       interactive=True)
+                num_inference_steps=gr.Slider(minimum=1, maximum=100, value=75, step=1,
+                                              label="Number of Inference Steps",
+                                              info="Selected how many steps the model takes to make the image higher "
+                                                   "quality. Takes longer for inference higher you make the number number",
+                                              interactive=True)
+                strength=gr.Slider(minimum=0, maximum=1, value=.75, step=.01,
+                                   label="Strength",
+                                   info="How much noise gets add to the photo or how much the photo changes.",
+                                   interactive=True)
+                guidance_scale=gr.Slider(minimum=0, maximum=1, value=.3, step=.01,
+                                         label="Guidance Scale",
+                                         info="How close the image should be to the original.",
+                                         interactive=True)
+                gr.Chatbot(height="50vh", show_label=False)
+                prompt = gr.Textbox(label="Image Prompt", placeholder="Enter image edit prompt...")
+            with gr.Column(scale=4, show_progress=True):
+                gr.Markdown("## <center>Output Image</center>")
+                gr.Image(height="70vh", show_label=False, interactive=False)
+        gr.Markdown("# <center>Output Image Gallery</center>")
+        gr.Tabs()
+        gr.Gallery(height="500",
+                   rows=[6],
+                   columns=[3],
+                   show_download_button=True,
+                   show_fullscreen_button=True,
+                   preview=True,
+                   label="Output Image Gallery",
+                   show_label=False,
+                   object_fit="contain")
+    with gr.Tab("Image to Image"):
+        with gr.Row():
+            with gr.Column(scale=4, show_progress=True):
+                gr.Markdown("## <center>Input Image</center>")
+                gr.Tabs()
+                gr.Image(height="50vh", show_label=False)
+            with gr.Column(scale=3, show_progress=True, variant="compact"):
+                t2i_num_images=gr.Slider(minimum=1, maximum=5, value=1, step=1,
+                                         label="Number of Images to Generate",
+                                         info="How many images you want the model to generate.",
+                                         interactive=True)
+                t2i_num_inference_steps=gr.Slider(minimum=1, maximum=100, value=75, step=1,
+                                                  label="Number of Inference Steps",
+                                                  info="Selected how many steps the model takes to make the image higher"
+                                                       " quality.Takes longer for inference higher you make the number"
+                                                       " number",
+                                                  interactive=True)
+                t2i_strength=gr.Slider(minimum=0, maximum=1, value=.75, step=.01,
+                                       label="Strength",
+                                       info="How much noise gets add to the photo or how much the photo changes.",
+                                       interactive=True)
+                t2i_guidance_scale=gr.Slider(minimum=0, maximum=1, value=.3, step=.01,
+                                             label="Guidance Steps",
+                                             info="How much noise gets add to the photo or how much the photo changes.",
+                                             interactive=True)
+                gr.Chatbot(height="30vh", show_label=False)
+                t2i_prompt = gr.Textbox(label="Image Prompt",placeholder="Enter image edit prompt...")
+            with gr.Column(scale=4, show_progress=True):
+                gr.Markdown("## <center>Output Image</center>")
+                gr.Tabs()
+                gr.Image(height="50vh", show_label=False, interactive=False)
+        gr.Markdown("# <center>Output Image Gallery</center>")
+        gr.Tabs()
+        gr.Gallery(height="30vh",
+                   rows=[6],
+                   columns=[3],
+                   show_download_button=True,
+                   show_fullscreen_button=True,
+                   preview=True,
+                   label="Output Image Gallery",
+                   show_label=False,
+                   object_fit="contain")
 
 
     demo.launch(inbrowser=True)
--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionXLPipeline
 from diffusers.utils import load_image
 import torch
+from torch.utils.tensorboard.summary import image
 
 """------------Text to Image Pipeline------------"""
 
-pipe = StableDiffusionPipeline.from_pretrained(
+pipe = StableDiffusionXLPipeline.from_pretrained(
     model="stabilityai/sdxl-turbo",
     device_map="auto",
     torch_dtype=torch.float16,
@@ -13,18 +14,45 @@ pipe = StableDiffusionPipeline.from_pretrained(
 
 prompt = ""
 
-textToImage = pipe(prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+textToImage = pipe(prompt, num_inference_steps=70,
+                   guidance_scale=0.0,
+                   num_images_per_prompt=1).images[0]
 
 """--------Image to Image Pipeline----------"""
 
-pipe = StableDiffusionPipeline.from_pretrained(
+base = StableDiffusionXLPipeline.from_pretrained(
     model="stabilityai/sdxl-turbo",
     device_map="auto",
     torch_dtype=torch.float16,
     use_safetensors=True,
     variant="fp16")
-init_image = load_image(image="")
+
+refiner = StableDiffusionXLPipeline.from_pretrained(
+    model= "stabilityai/stable-diffusion-xl-refiner-1.0",
+    device_map="auto",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16"
+)
+
+init_image = load_image(image="") # original image
 
 prompt = ""
 
-imageToImage = pipe(prompt, image=init_image, num_inference_steps=2, strength=0.5, guidance_scale=0.0).images[0]
+imageToImage = base(prompt,
+                    image=init_image,
+                    num_inference_steps=75, # how many steps the model takes to make the image higher quality, takes longer for inference higher the number
+                    strength=0.3, # more noise that gets added to the image or how much the image gets changed
+                    guidance_scale=0.3,  # How close the image should be to the original
+                    denoising_end=.75,
+                    num_images_per_prompt=3, # how many images get generated
+                    output_type="latent"
+                    ).images[0]
+imageToImage = refiner(prompt,
+                       image=imageToImage,
+                       num_inference_steps=75,
+                       strength=.3,
+                       guidance_scale=0.3,
+                       denoising_start=.75,
+                       num_images_per_prompt=3
+                       ).images[0]
