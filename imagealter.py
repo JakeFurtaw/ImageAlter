f77ec050af3049dea40c8d7069c232b1950d80c0
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-09-01 23:47:57 -0400

    changed gallery specs

--- imagealter.py
+++ imagealter.py
@@ -14,23 +14,25 @@ with gr.Blocks(theme="default", fill_width=True, css=css) as demo:
         with gr.Column(scale=4, show_progress=True):
             gr.Markdown("## <center>Input Image</center>")
             gr.Tabs()
-            gr.Image(height="50vh")
+            gr.Image(height="50vh", show_label=False)
         with gr.Column(scale=3, show_progress=True, variant="compact"):
             gr.Chatbot(height="50vh")
             gr.Textbox(placeholder="Enter image edit prompt...")
         with gr.Column(scale=4, show_progress=True):
             gr.Markdown("## <center>Output Image</center>")
             gr.Tabs()
-            gr.Image(height="50vh", show_download_button=True, show_fullscreen_button=True, interactive=False)
+            gr.Image(height="50vh", show_label=False, interactive=False)
     gr.Markdown("# <center>Output Image Gallery</center>")
+    gr.Tabs()
     gr.Gallery(height="500",
-               rows=[3],
-               columns=[3],
+               rows=[6],
+               columns=[6],
                show_download_button=True,
                show_fullscreen_button=True,
-               show_share_button=True,
                preview=True,
-               label="Output Image Gallery")
+               label="Output Image Gallery",
+               show_label=False,
+               object_fit="scale-down")
 
 
     demo.launch(inbrowser=True)
